f9ad3132b0a529f8eabfae76ca33b6f4ed882205
AnuragOP01 <ranurag404@gmail.com>
2025-07-09 10:32:57 +0000

    exported letter head script

--- eits_app/hooks.py
+++ eits_app/hooks.py
@@ -13,7 +13,7 @@ fixtures = [
 # Apps
 # ------------------
 
-fixtures = ["Server Script" ,"Client Script","Print Format" ]
+fixtures = ["Server Script" ,"Client Script","Print Format","Letter Head" ]
 
 
 # required_apps = []
