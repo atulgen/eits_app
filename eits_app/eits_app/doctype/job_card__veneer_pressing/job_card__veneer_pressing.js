044e7f97ef7c8dbc8172889a34e5c10daff5ddf5
Aditya Verma <verma.adii03@example.com>
2025-07-18 16:45:35 +0530

    added job card -veneer and other services and completed the dwa

---
+++ eits_app/eits_app/doctype/job_card__veneer_pressing/job_card__veneer_pressing.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, GennextIT and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Job Card -Veneer Pressing", {
+// 	refresh(frm) {
+
+// 	},
+// });
