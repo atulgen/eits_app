93e84bcc0c39d889d03cafa8fbdf650e76a33ded
atulgen <atul.raj@gennextit.com>
2025-08-25 09:47:59 +0530

    Reduced time to 17:45

--- eits_app/inspector_availability.py
+++ eits_app/inspector_availability.py
@@ -259,7 +259,7 @@ def calculate_free_slots(occupied_slots, date):
     """
     # Define working hours (9:00 AM to 6:00 PM)
     shift_start = datetime.combine(date, time(9, 0))  # 9:00 AM
-    shift_end = datetime.combine(date, time(18, 0))   # 6:00 PM
+    shift_end = datetime.combine(date, time(17, 45))   # 6:00 PM
     
     free_slots = []
     
